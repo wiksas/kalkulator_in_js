d369ba757bfc82c18b7351020f6eb8299b9052df
wiksas <wiktor.sasnal04@gmail.com>
2021-04-24 19:33:52 +0200

    script

---
+++ script.js
@@ -0,0 +1,34 @@
+let wynik;
+let numer=0;
+
+
+
+
+
+function numer(){
+    numer=i;
+}
+
+
+function myfunction(i){
+    if{i==1}{
+        wynik=document.getElementById(pierwszaliczba)+document.getElementById(drugaliczba);
+    }else if(i==2){
+        wynik=document.getElementById(pierwszaliczba)-document.getElementById(drugaliczba);
+    }else if(i==3){
+        wynik=document.getElementById(pierwszaliczba)*document.getElementById(drugaliczba);
+    }else if(i==4){
+        wynik=document.getElementById(pierwszaliczba)/document.getElementById(drugaliczba);
+    }else if(i==5){
+        wynik=Math.pow(document.getElementById(pierwszaliczba), document.getElementById(drugaliczba));
+    }else if(i==0){
+        document.getElementById(wynik).textContent = 'najpierw wybierz działanie';
+    }
+
+
+
+}
+
+
+
+
